b6e6491e19a8a9fb98af96895962665b212b8cf7
sergio-salazar <sergio.salazarmon@gmail.com>
2019-11-21 22:50:36 -0600

    Actualización de código.

---
+++ app/src/main/java/mx/edu/itl/c16130842/canyouhelpmebeta/ContactosActivity.java
@@ -0,0 +1,39 @@
+package mx.edu.itl.c16130842.canyouhelpmebeta;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class ContactosActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private RecyclerView.LayoutManager layoutManager;
+    private RecyclerView.Adapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contactos);
+
+        recyclerView = findViewById(R.id.recycler_view_llamada);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        adapter = new AdaptadorRecyclerViewContactos(ContactosActivity.this);
+        recyclerView.setAdapter(adapter);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
--- app/src/main/java/mx/edu/itl/c16130842/canyouhelpmebeta/MainActivity.java
+++ app/src/main/java/mx/edu/itl/c16130842/canyouhelpmebeta/MainActivity.java
@@ -1,7 +1,11 @@
 package mx.edu.itl.c16130842.canyouhelpmebeta;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public String[] contactos = new String[] {"Jessica", "Monica", "Ivan", "Emergencia - 911"};
+
+    public void btnEmergenciaOnClick(View view) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("LLAMAR A: ");
+        builder.setSingleChoiceItems(contactos, 1, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+    }
+
+    public void btnMedicamentosOnClick(View view) {
+
+    }
+
+    public void btnConfiguracionOnClick(View view) {
+        Intent intent = new Intent(MainActivity.this, ConfiguracionActivity.class);
+        startActivity(intent);
     }
 }
