4ca1fd1bf4d0851e0593e48fc1401597114ae6ef
PaperPlaneSolutionsGPS <56463508+PaperPlaneSolutionsGPS@users.noreply.github.com>
2019-12-04 14:53:41 -0600

    Comentarios

--- app/src/main/java/mx/edu/itl/c16130842/canyouhelpmebeta/ConfiguracionActivity.java
+++ app/src/main/java/mx/edu/itl/c16130842/canyouhelpmebeta/ConfiguracionActivity.java
@@ -1,3 +1,22 @@
+/*------------------------------------------------------------------------------------------
+:*                       INSTITUTO TECNOLOGICO DE LA LAGUNA
+:*                     INGENIERIA EN SISTEMAS COMPUTACIONALES
+:*                       GESTIÓN DE PROYECTOS DE SOFTWARE
+:*
+:*                   SEMESTRE: AGO-DIC/2019    HORA: 11-12 HRS
+:*
+:*                       Activity de configuración
+:*
+:*  Archivo     : ConfiguracionActivity.java
+:*  Autor       : PPS
+:*  Compilador  : Android Studio 3.1.3
+:*  Descripción : Activity que solamente actúa como "intermediario" entre nuestras pantallas de
+:*  configuración
+:*  de teléfono. 
+:*  Fecha       Modificó             Motivo
+:*==========================================================================================
+:*  04/012/2019 Iván García Moreno   Comentarios
+:*------------------------------------------------------------------------------------------*/
 package mx.edu.itl.c16130842.canyouhelpmebeta;
 
 import android.content.Intent;
@@ -7,6 +26,10 @@ import android.view.View;
 
 public class ConfiguracionActivity extends AppCompatActivity {
 
+    /*
+     * Método estándar que se tiene que sobreescribir, infla el layout y oculta
+     * la ActioBar para hacer más fácil la vista.
+    */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,12 +37,14 @@ public class ConfiguracionActivity extends AppCompatActivity {
 
         getSupportActionBar().hide();
     }
-
+    
+    // En caso de click en este método, iniciamos el Activity de selección de contactos
     public void btnConfigurarContactosOnClick(View view) {
         Intent intent = new Intent(ConfiguracionActivity.this, ContactosActivity.class);
         startActivity(intent);
     }
 
+    // En caso de click en el otro botón, abrimos el activity para personalziar el SMS
     public void btnConfigurarMensajeOnClick(View view) {
         Intent intent = new Intent(ConfiguracionActivity.this, SMSActivity.class);
         startActivity(intent);
