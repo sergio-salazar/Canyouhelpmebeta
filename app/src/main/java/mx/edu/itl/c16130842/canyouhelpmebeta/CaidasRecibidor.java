7af02e223fdbb01b2ab356862efe552e340e9de0
PaperPlaneSolutionsGPS <56463508+PaperPlaneSolutionsGPS@users.noreply.github.com>
2019-12-04 11:40:23 -0600

    Comentarios CaidasRecibidor.java
    Recibidor de Broadcast de las caídas, comentarios acorde con el estándar. Realizados por Iván García, el 04/12 a las 11:40 AM

--- app/src/main/java/mx/edu/itl/c16130842/canyouhelpmebeta/CaidasRecibidor.java
+++ app/src/main/java/mx/edu/itl/c16130842/canyouhelpmebeta/CaidasRecibidor.java
@@ -1,3 +1,21 @@
+/*------------------------------------------------------------------------------------------
+:*                       INSTITUTO TECNOLOGICO DE LA LAGUNA
+:*                     INGENIERIA EN SISTEMAS COMPUTACIONALES
+:*                       GESTIÓN DE PROYECTOS DE SOFTWARE
+:*
+:*                   SEMESTRE: AGO-DIC/2019    HORA: 11-12 HRS
+:*
+:*                       Recibidor de Broadcast al momento de la caída
+:*
+:*  Archivo     : CaidasRecibidor.java
+:*  Autor       : PPS
+:*  Compilador  : Android Studio 3.1.3
+:*  Descripción : Clase recibe el Broadcast emitido al momento de la caída y hace la llamada
+:*  de teléfono. 
+:*  Fecha       Modificó             Motivo
+:*==========================================================================================
+:*  04/012/2019 Iván García Moreno   Comentarios
+:*------------------------------------------------------------------------------------------*/
 package mx.edu.itl.c16130842.canyouhelpmebeta;
 
 import android.Manifest;
@@ -10,6 +28,10 @@ import android.support.v4.app.ActivityCompat;
 
 public class CaidasRecibidor extends BroadcastReceiver {
 
+    /*
+     * Sobreescritura del método onReceive. Este método es llamado por el sistema Android cuando
+     * se detecta la caída, y llama al número correspondiente.
+     */
     @Override
     public void onReceive(Context context, Intent intent) {
         Intent i = new Intent(Intent.ACTION_CALL);
